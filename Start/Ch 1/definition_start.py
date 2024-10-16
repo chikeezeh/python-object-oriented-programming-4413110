7339f4ee5af61574115253693dc9fc0363eab511
Chike Ezeh <ezeh.chike@gmail.com>
2024-10-16 21:40:33 +0000

    basic class definition complete

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
+class Book:
+  def __init__(self, name) -> None:
+     self.name = name
 
+  def __repr__(self) -> str:
+      return f"This is a Book class with name = {self.name}"
 
 # TODO: create instances of the class
-
+book1 = Book("LOTR")
 
 # TODO: print the class and property
+print(book1)
