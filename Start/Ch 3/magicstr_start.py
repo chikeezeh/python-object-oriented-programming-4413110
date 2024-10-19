c4c078d89de1fe9fa0d7406ad60463bb45b91068
Chike Ezeh <ezeh.chike@gmail.com>
2024-10-19 02:14:19 +0000

    magic str completed.

--- Start/Ch 3/magicstr_start.py
+++ Start/Ch 3/magicstr_start.py
@@ -10,12 +10,20 @@ class Book:
         self.price = price
 
     # TODO: use the __str__ method to return a string
-
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}, costs ${self.price}"
     # TODO: use the __repr__ method to return an obj representation
-
+    def __repr__(self) -> str:
+        return f"Book({self.title}, {self.author}, {self.price})"
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 print(b1)
 print(b2)
+
+print(str(b1))
+print(str(b2))
+
+print(repr(b1))
+print(repr(b2))
